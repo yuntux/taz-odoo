525cb98ffdca8226f7a606a50a67e82cd2690f4d
Aurélien Dumaine <yuntux@gmail.com>
2026-09-13 03:21:24 +0200

    Update l10n_fr_siret

--- l10n_fr_siret/models/res_partner.py
+++ l10n_fr_siret/models/res_partner.py
@@ -1,7 +1,7 @@
 import logging
 
-from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
+from odoo import _, api, fields, models, tools
+from odoo.exceptions import UserError, ValidationError
 
 logger = logging.getLogger(__name__)
 try:
@@ -53,6 +53,13 @@ class Partner(models.Model):
         string="Partners with same SIREN",
         compute_sudo=True,
     )
+    # Field below is identical to the field on res.company defined in l10n_fr.
+    # It is different from fiscal_country_codes defined in the 'account' module
+    # which takes into account both the country of the company
+    # AND the country of the partner
+    is_france_country = fields.Boolean(
+        compute="_compute_is_france_country",
+    )
 
     @api.depends("siren", "nic")
     def _compute_siret(self):
@@ -103,7 +110,16 @@ class Partner(models.Model):
                 ).ids or False
             partner.same_siren_partner_ids = same_siren_partner_ids
 
-    @api.constrains("siren", "nic")
+    @api.depends("country_id")
+    def _compute_is_france_country(self):
+        fr_country_codes = self.env["res.company"]._get_france_country_codes()
+        for partner in self:
+            is_france_country = False
+            if partner.country_id and partner.country_id.code in fr_country_codes:
+                is_france_country = True
+            partner.is_france_country = is_france_country
+
+    @api.constrains("siren", "nic", "vat")
     def _check_siret(self):
         """Check the SIREN's and NIC's keys (last digits)"""
         for rec in self:
@@ -144,6 +160,23 @@ class Partner(models.Model):
                             siret=(rec.siren + rec.nic), partner_name=rec.display_name
                         )
                     )
+                if rec.vat:
+                    vat = "".join(x for x in rec.vat if not x.isspace())
+                    # vat numbers have no spaces when base_vat is installed
+                    # but installation of base_vat doesn't remove spaces in vat numbers
+                    # encoded before the installation of the module
+                    if vat and vat.startswith("FR") and not vat.endswith(rec.siren):
+                        raise ValidationError(
+                            _(
+                                "On partner '%(partner_name)s', "
+                                "the VAT number %(vat)s is not consistent with "
+                                "SIREN %(siren)s: a french VAT number has the 9 "
+                                "digits of SIREN at the end.",
+                                partner_name=rec.display_name,
+                                siren=rec.siren,
+                                vat=vat,
+                            )
+                        )
 
     @api.model
     def _commercial_fields(self):
@@ -176,3 +209,60 @@ class Partner(models.Model):
             "target": "current",
         }
         return action
+
+    def _get_siren(self, raise_if_none=False):
+        self.ensure_one()
+        partner = self.parent_id or self
+        running_env = tools.config.get("running_env")
+        # Hack for SUPER PDP sandbox because, unfortunately,
+        # SUPER PDP demo SIRENs don't have a compliant checksum
+        if running_env in ("test", "dev") and partner.siren in (
+            "000000001",
+            "000000002",
+        ):
+            return partner.siren
+        if partner.vat:
+            vat = "".join(x for x in partner.vat if not x.isspace())
+            if (
+                vat
+                and vat.startswith("FR")
+                and len(vat) == 13
+                and siren.is_valid(vat[4:])
+            ):
+                return vat[4:]
+        if partner.siren and siren.is_valid(partner.siren):
+            return partner.siren
+        if raise_if_none:
+            raise UserError(
+                self.env._(
+                    "SIREN is not set on partner '%(partner)s'.",
+                    partner=partner.display_name,
+                )
+            )
+        return None
+
+    def _get_siret(self, raise_if_none=False):
+        self.ensure_one()
+        if self.siren and self.nic and siret.is_valid(self.siret):
+            return self.siret
+        if raise_if_none:
+            raise UserError(
+                self.env._(
+                    "SIRET is not set on partner '%(partner)s'.",
+                    partner=self.display_name,
+                )
+            )
+        return None
+
+    def _get_nic(self, raise_if_none=False):
+        self.ensure_one()
+        if self.siren and self.nic and siret.is_valid(self.siret):
+            return self.nic
+        if raise_if_none:
+            raise UserError(
+                self.env._(
+                    "NIC is not set on partner '%(partner)s'.",
+                    partner=self.display_name,
+                )
+            )
+        return None
